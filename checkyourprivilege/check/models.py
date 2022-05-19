a19fca0bcce483b8805d5ca6cad8f4278af23807
Julian Stieß <j_stiess17@cs.uni-kl.de>
2022-05-19 20:03:46 +0200

    initial commit

---
+++ checkyourprivilege/check/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.crypto import get_random_string
+import datetime
+# Create your models here.
+class Privilege(models.Model):
+    
+    privilege_owner = models.CharField(max_length=12, default=lambda: get_random_string(12).upper(), editable=False)
+    check_count = models.BigIntegerField(default=1)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def checked_today(self):
+        return datetime.datetime.today().date() == self.updated_at.date()
+
+    def check_privilege(self):
+        print("updating bitch") 
+        print(datetime.datetime.today().date())
+        print(self.updated_at.date())
+        if not self.checked_today():
+            self.check_count += 1
+            print("UPDATED BITCH")
+            return True
+        return False
+    
